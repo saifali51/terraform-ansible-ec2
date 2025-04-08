cee2fd1fbae6c3ea1471e7cc3503d2de5db1ad22
saif ali <saifali01270@gmail.com>
2025-04-09 00:49:49 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,15 @@ resource "aws_security_group" "ssh" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  } 
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+
 
   egress {
     from_port   = 0
