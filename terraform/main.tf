56813ad7c6e550a088c5fa707e5435788499db23
saif ali <saifali01270@gmail.com>
2025-04-09 00:13:52 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,7 +62,7 @@ resource "aws_security_group" "ssh" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 in us-east-1
+  ami           = "ami-00a929b66ed6e0de6" 
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.ssh.id]
